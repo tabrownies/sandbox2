7c00d7c2c2751f3541b526da12a7c93955c4c301
Timothy Brown <tabrownies@gmail.com>
2021-11-19 18:59:37 -0700

    Preparing to build with javascript

---
+++ expenses.md
@@ -0,0 +1,10 @@
+### Expenses
+rent (monthly constant)
+food (weekly variable)
+date night (one time fun)
+transportation (weekly variable)
+tuition (once a year)
+
+### Income
+weekly paycheck
+summer sales
--- practice.py
+++ practice.py
@@ -1,6 +1,67 @@
+# for simplification I'm going a year out and planning for each quarter month
+
+# types of regularness
+frequencies = ['monthly','yearly']
+one_time_expense_types = ['fun','budget','emergency']
+scheduled_expense_types = ['definite', 'variable']
+
+#types of expenses and income
+class Expense():
+    # if date is in future this a future scheduled expense
+    # if date is in past this is an expense that has occured
+    def __init__(self, title, amount, date, expense_type):
+        self.title = title
+        self.amount = amount
+        self.date = date
+        self.expense_type = expense_type
+
+class Repeat_Expense(Expense):
+    def __init__(self, title, amount, expense_type, frequency):
+        super().__init__(title, amount, 0, expense_type)
+        # if frequency not in frequencies:
+        #     raise ValueError("frequency not known")
+        self.frequency = frequency
+
+class Income():
+    # if the date is in the future this a scheduled event
+    def __init__(self, title, amount, date):
+        self.title = title
+        self.amount = amount
+        self.date = date
+
+class Repeat_Income(Income):
+    def __init__(self, title, amount, frequency):
+        super().__init__(title, amount, 0)
+        self.frequency = frequency
+
+# fake data
+expenses = []
+income = []
+
+# rent (monthly constant)
+expenses.append(Repeat_Expense('rent',400,'definite','monthly'))
+# food (weekly variable)
+expenses.append(Repeat_Expense('food',200,'variable','monthly'))
+# date night (one time fun)
+# expenses.append(Expense('date', 30,4,'fun'))
+# transportation (monthly variable)
+expenses.append(Repeat_Expense('transportation',150,'variable','monthly'))
+# tuition (twice a year)
+expenses.append(Expense('tuition', 5000,3,'budget'))
+expenses.append(Expense('tuition', 5000,9,'budget'))
+
+# monthly paycheck
+income.append(Repeat_Income('TA job',800,'monthly'))
+# summer sales
+income.append(Income('Summer Sales',30000,7))
+
 # goals
 rainy_goal = 1000
 savings_goal = 10000
+
+#amount to be put aside
+toward_rainy = 50
+towards_savings = 300
 
 # current amounts
 fun_amount = 0
@@ -11,51 +72,40 @@ rainy_amount = 350
 budget = 0
 fun = 0
 
-# types of regularness
-frequencies = ['daily','weekly','monthly','yearly']
-one_time_expense_types = ['fun','budget','emergency']
-scheduled_expense_types = ['definite', 'variable']
+# algorithm testing
+def calculate_yearly_plan():
+    # prep varables
+    savings = [0]*12
+    rainy_day = [0]*12
 
-#types of expenses and income
-class Expense():
-    # if date is in future this a future scheduled expense
-    # if date is in past this is an expense that has occured
-    def __init__(self, amount, date, expense_type):
-        self.amount = amount
-        self.date = date
-        self.expense_type = expense_type
+    monthly_expenses = [0]*12
+    # print(monthly_expenses)
+    monthly_income = [0]*12
 
-# class One_Time_Expense(Expense):
-#     def __init__(self, amount, date):
-#         super().__init__(self,amount,date)
+    # initialize values
+    savings[0] = savings_amount
+    rainy_day[0] = rainy_amount
 
-class Repeat_Expense(Expense):
-    def __init__(self, amount, date, expense_type, frequency):
-        super().__init__(amount, date, expense_type)
-        if frequency not in frequencies:
-            raise ValueError("frequency not known")
-        self.frequency = frequency
+    for expense in expenses:
+        # filter expenses
+        print(expense.expense_type)
+        if expense.expense_type != 'monthly':
+            monthly_expenses[expense.date]+=expense.amount
+        else:
+            for i, value in enumerate(monthly_expenses):
+                monthly_expenses[i] += expense.amount
+    print(monthly_expenses)
 
-# class Scheduled_Expense(Expense):
-#     def __init__(self, amount, date):
-#         super().__init__(amount, date)
 
-# class Rainy_Day(One_Time):
-#     pass
 
-# class Fun(One_Time):
-#     pass
 
-class Income():
-    pass
 
-class Scheduled_Income(Income):
-    pass
 
-class One_Time_Income(Income):
-    pass
 
-class Repeat_Income(Scheduled_Income):
-    pass
+
+
+calculate_yearly_plan()
+
+
 
 
